7839d9e4076afde431df6017a36f08ed808e21a4
Islam Sayed <islam.sayed8@gmail.com>
2020-01-11 14:37:00 +0200

    add join method code

---
+++ join/app.js
@@ -0,0 +1,22 @@
+/********Array join() method*********/
+//joins elements of array in a string.
+//returns the new string.
+//sperator between former array elements is specified by the
+//method string arg.
+
+//Examples:
+const arr = ["Hello", "World"]
+arr.join();
+
+/* const arr = ["Hello", "World"];
+arr.join(', '); */
+
+/* const arr = ["Hello", "World"];
+arr.join(''); */
+
+/* const arr = ["Hello", "World"];
+arr.join(' '); */
+
+/* const arr = ["Hello", "World"];
+arr.join('-'); */
+
