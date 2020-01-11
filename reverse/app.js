42e63768327cfb11c42955f679b879c8e78d3f9d
Islam Sayed <islam.sayed8@gmail.com>
2020-01-11 14:37:13 +0200

    add reverse method code

---
+++ reverse/app.js
@@ -0,0 +1,8 @@
+/********Array reverse() method*********/
+//reverses array in place(mutate the array).
+//returns the reversed array.
+
+//Examples:
+
+const arr = [1, 2, 3, 4]
+arr.reverse();//[4, 3, 2, 1]
